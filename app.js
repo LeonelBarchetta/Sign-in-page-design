3a933548b6419522698611c75d53fca0bc138402
unknown <barchettaleonel@gmailcom>
2023-12-29 19:14:43 -0300

    nuevo5

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const password = document.querySelector(".password")
 const ojo = document.querySelector(".ojo")
+const ojo2 =document.querySelector(".ojo2")
 
 ojo.addEventListener("click",function(){
     if(password.type=="password"){
         password.type= "text"
-    }else{
-        password.type="password"
+        ojo.style.display = "none";
+        ojo2.style.display = "block";
+
+    }
+    // else{
+    //     password.type="password"
+    // }
+})
+
+ojo2.addEventListener("click",function(){
+    if(password.type=="text"){
+        password.type="password";
+        ojo2.style.display= "none";
+        ojo.style.display= "block";
     }
 })
